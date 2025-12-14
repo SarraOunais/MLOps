b6235f37da09427d750e8e7af71b05b0095fa3f4
SarraOunais <sarra.ounais@esprit.tn>
2025-12-14 13:22:16 +0100

    Docker

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ def init_db(path):
 init_db(DB_PATH)
 
 def main():
-    #mlflow.set_tracking_uri("sqlite:///mlflow.db")
-    mlflow.set_tracking_uri("sqlite:///{DB_PATH}")
+    
+    mlflow.set_tracking_uri(f"sqlite:///{DB_PATH}")
 
     # Chemins des fichiers de données
     train_path = "data/train.csv"
